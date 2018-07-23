5e041f6a43066f106b11a19d1225f33f10ff07c9
raeesaroj <raesaroj16@gmail.com>
2018-07-23 15:28:30 +0545

    crud operations for mahila pratinidhi form

--- mahila_pratinidhi/core/models.py
+++ mahila_pratinidhi/core/models.py
@@ -4,25 +4,28 @@ from django.db import models
 class District(models.Model):
 	name = models.CharField(max_length=300)
 
+	def __str__(self):
+		return self.name
+
 
 class MahilaPratinidhiForm(models.Model):
-	district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='district')
-	name = models.CharField(max_length=300, verbose_name="कार्यक्रम")
-	age = models.IntegerField()
-	marital_status = models.CharField(max_length=300)
-	educational_qualification = models.CharField(max_length=300)
-	caste = models.CharField(max_length=300)
-	address = models.CharField(max_length=300)
-	contact_number = models.IntegerField()
-	email = models.EmailField()
-	nirwachit_padh = models.CharField(max_length=300)
-	nirwachit_vdc_or_municipality_name = models.CharField(max_length=300)
-	party_name = models.CharField(max_length=300)
-	party_joined_date = models.DateField()
-	samlagna_sang_sastha_samuha = models.CharField(max_length=300)
-	nirwachit_chetra_pratiko_pratibadhata = models.TextField()
+	district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='district', verbose_name="जिल्ला")
+	name = models.CharField(max_length=300, verbose_name="नाम")
+	age = models.CharField(max_length=300, verbose_name="उमेर")
+	marital_status = models.CharField(max_length=300, verbose_name="बैवाहिक स्थिथि")
+	educational_qualification = models.CharField(max_length=300, verbose_name="शैछिक योग्यता")
+	caste = models.CharField(max_length=300, verbose_name="जातियता")
+	address = models.CharField(max_length=300, verbose_name="ठेगाना")
+	contact_number = models.CharField(max_length=300, verbose_name="सम्पर्क न.")
+	email = models.EmailField(verbose_name="इ-मेल")
+	nirwachit_padh = models.CharField(max_length=300, verbose_name="निर्वाचित पद")
+	nirwachit_vdc_or_municipality_name = models.CharField(max_length=300, verbose_name="निर्वाचित गा.बि.स वा नगरपालिकाको नाम")
+	party_name = models.CharField(max_length=300, verbose_name="पार्टीको नाम")
+	party_joined_date = models.CharField(max_length=300, verbose_name="पार्टीमा संलग्न भएको मिति")
+	samlagna_sang_sastha_samuha = models.CharField(max_length=300, verbose_name="संलग्न संग सस्था समूह")
+	nirwachit_chetra_pratiko_pratibadhata = models.TextField(verbose_name="निर्वाचित क्षेत्र प्रतिको प्रतिबध्धता")
+
+	def __str__(self):
+		return "{} फारम".format(self.district.name)
 
 
-class MahilaPratinidhiFile(models.Model):
-	district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='file_district')
-	file = models.FileField(upload_to='district/')
--- mahila_pratinidhi/core/views.py
+++ mahila_pratinidhi/core/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic import CreateView, ListView, UpdateView, DetailView, DeleteView
+
+from .models import MahilaPratinidhiForm
+from .forms import MahilaPratinidhiFormForm
+
+
+class MahilaPratinidhiFormCreateView(CreateView):
+
+	model = MahilaPratinidhiForm
+	form_class = MahilaPratinidhiFormForm
+	success_url = reverse_lazy('core:mahila_pratinidhi_list')
+	template_name = "core/mahila_pratinidhi_form.html"
+
+
+class MahilaPratinidhiFormListView(ListView):
+
+	model = MahilaPratinidhiForm
+	template_name = "core/mahila_pratinidhi_list.html"
+
+
+class MahilaPratinidhiFormDetailView(DetailView):
+
+	model = MahilaPratinidhiForm
+	template_name = "core/mahila_pratinidhi_detail.html"
+
+
+class MahilaPratinidhiFormUpdateView(UpdateView):
+
+	model = MahilaPratinidhiForm
+	form_class = MahilaPratinidhiFormForm
+	success_url = reverse_lazy('core:mahila_pratinidhi_list')
+	template_name = "core/mahila_pratinidhi_form.html"
+
+
+class MahilaPratinidhiFormDeleteView(DeleteView):
+
+	model = MahilaPratinidhiForm
+	success_url = reverse_lazy('core:mahila_pratinidhi_list')
+	template_name = "core/mahila_pratinidhi_delete.html"
+
+
