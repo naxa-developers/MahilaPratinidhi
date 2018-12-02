176e66b2fc623bc788cf36b19545a1c7b6edf445
sanip <saneprijal@gmail.com>
2018-12-02 19:47:01 +0545

    login and signup

--- mahila_pratinidhi/mahila_pratinidhi/settings.py
+++ mahila_pratinidhi/mahila_pratinidhi/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login'
+LOGOUT_REDIRECT_URL = 'public:signup'
 
 LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale'),
