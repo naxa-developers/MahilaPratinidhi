aea80ef0a68f1b4ad71029bfe5d2bdc4abc8fb79
SanipRijal <saneprijal@gmail.com>
2018-12-07 12:06:25 +0545

    maps api

--- mahila_pratinidhi/mahila_pratinidhi/settings.py
+++ mahila_pratinidhi/mahila_pratinidhi/settings.py
@@ -160,6 +160,7 @@ except Exception as e:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'saneprijal@gmail.com'
-EMAIL_HOST_PASSWORD = '9841759355'
+#server mail credentials
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
