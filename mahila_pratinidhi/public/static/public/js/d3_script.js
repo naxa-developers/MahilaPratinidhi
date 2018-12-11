92b6ef513b141227f363064d152f827d99ec98e6
lomashbhattarai <lomashbhattarai1@gmail.com>
2018-12-11 10:46:58 +0545

    map

--- mahila_pratinidhi/public/static/public/js/d3_script.js
+++ mahila_pratinidhi/public/static/public/js/d3_script.js
@@ -1,9 +1,27 @@
 //alert("pasyo");
 //var base_url="http://mahilapratinidhi.naxa.com.np";
 var base_url="http://localhost:8000";
+$.get(base_url+'/api/age/',function(data){
+
+
+    ReactDOM.render(
+        <div>
+        <ChartBox data_pass={[{'title':'age','data-type':'0','dataName':'total','data':data['total']}]} />
+        <ChartBox data_pass={[{'title':'age' +" vs Party",'data-type':'1','dataName':'party','data':data['party']}]} />
+        <ChartBox data_pass={[{'title':'age' +" vs Province",'data-type':'1','dataName':'provincial','data':data['provincial']}]} />
+        </div>,
+        document.getElementById("react-container")
+      )
+
+
+
+});
+
+
 $(".dataVariable").on("click",function(){
 
   var variable = $(this).text().toLowerCase().replace(" ","_");
+
   $.get(base_url+'/api/'+ variable +'/',function(data){
 
 
